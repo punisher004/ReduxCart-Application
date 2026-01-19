6fc8aa1ec81d7c82f0453fb04471e1552e2bdefa
punisher <muahammad@gamil.com>
2026-01-19 06:35:07 +0300

    cart-slice file codes

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
     name: 'ui',
-    initialState: { cartIsVisable: false },
+    initialState: { cartIsVisible: false },
     reducers: {
         toggle(state) {
         state.cartIsVisible = !state.cartIsVisible;
