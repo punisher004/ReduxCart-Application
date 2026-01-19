58736dfe495b54f92030bf6b14237e2827dd4ff1
punisher <muahammad@gamil.com>
2026-01-19 06:44:33 +0300

    merge cart-slice into overall Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { configure } from "@testing-library/react";
+import { configureStore } from "@testing-library/react";
 
 import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-    reducer: { ui: uiSlice.reducer }
+    reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
